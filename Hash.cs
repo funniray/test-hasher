b6a1f49b981b78c79ac8961053cd40956343fe44
funniray <funniray10@gmail.com>
2023-07-06 22:30:00 +0100

    Make other hashing algos optional

--- Hash.cs
+++ Hash.cs
@@ -19,7 +19,7 @@ namespace HashTest {
             timers[timerIndex] += timer.ElapsedMilliseconds;
         }
 
-        public void Hash(string filename) {
+        public void Hash(string filename, bool crcEnabled, bool sha1Enabled, bool md5Enabled) {
             timers = new double[3]{0,0,0};
             var timer = System.Diagnostics.Stopwatch.StartNew();
             using (var stream = File.Open(filename, FileMode.Open)) {
@@ -32,23 +32,24 @@ namespace HashTest {
                     using var sha1 = SHA1.Create();
                     using var md5 = MD5.Create();
 
-                    Task? crcTask = null;
-                    Task? sha1Task = null;
-                    Task? md5Task = null;
+                    Task crcTask = Task.Run(()=>{});
+                    Task sha1Task = Task.Run(()=>{});
+                    Task md5Task = Task.Run(()=>{});
                     
                     while (true) {
                         byte[] read = reader.ReadBytes(ED2K_BLOCK_LENGTH);
-                        if (crcTask != null && sha1Task != null && md5Task != null) {
-                            Task.WaitAll(new Task[3]{crcTask, sha1Task, md5Task});
-                        }
+                        Task.WaitAll(new Task[3]{crcTask, sha1Task, md5Task});
 
                         if (read.Length <= 0) {break;}
                         var localIndex = i;
                         var isLast = read.Length < ED2K_BLOCK_LENGTH;
                     
-                        crcTask = Task.Run(()=>{hashBlock(crc, read, isLast, 0);});
-                        sha1Task = Task.Run(()=>{hashBlock(sha1, read, isLast, 1);});
-                        md5Task = Task.Run(()=>{hashBlock(md5, read, isLast, 2);});
+                        if (crcEnabled)
+                            crcTask = Task.Run(()=>{hashBlock(crc, read, isLast, 0);});
+                        if (sha1Enabled)
+                            sha1Task = Task.Run(()=>{hashBlock(sha1, read, isLast, 1);});
+                        if (md5Enabled)
+                            md5Task = Task.Run(()=>{hashBlock(md5, read, isLast, 2);});
 
                         tasks.Add(Task.Run(()=>{
                             MD4 md4 = new MD4();
@@ -74,12 +75,15 @@ namespace HashTest {
 
                     var file = new FileInfo(filename);
 
-                    Console.WriteLine("CRC32: {0} (took {1}ms)", MD4.BytesToHex(crc.Hash, null), timers[0]);
-                    Console.WriteLine("SHA1: {0} (took {1}ms)", MD4.BytesToHex(sha1.Hash, null), timers[1]);
-                    Console.WriteLine("MD5: {0} (took {1}ms)", MD4.BytesToHex(md5.Hash, null), timers[2]);
+                    if (crcEnabled)
+                        Console.WriteLine("CRC32: {0} (took {1}ms)", MD4.BytesToHex(crc.Hash, null), timers[0]);
+                    if (sha1Enabled)
+                        Console.WriteLine("SHA1: {0} (took {1}ms)", MD4.BytesToHex(sha1.Hash, null), timers[1]);
+                    if (md5Enabled)
+                        Console.WriteLine("MD5: {0} (took {1}ms)", MD4.BytesToHex(md5.Hash, null), timers[2]);
                     Console.WriteLine("ED2K: {0}", hash);
 
-                    Console.WriteLine("Overall speed: {0}MB/s in {1}ms", (file.Length/(1024*1024))/(time/1000), time);
+                    Console.WriteLine("Overall speed: {0}MB/s in {1}ms", Math.Round((file.Length/(1024*1024))/(time/1000.0), 2), time);
                 }
             }
         }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿namespace HashTest {
     class Program {
-        static void Main(string[] args) {
+        static int Main(string[] args) {
+            if (args.Length == 0) {
+                Console.WriteLine("Usage: testFile [--crc | -C] [--sha1 | -S] [--md5 | -M] [--all] file");
+                return 1;
+            }
+
+            bool enableCRC = args.Contains("--crc") || args.Contains("-C");
+            bool enableSHA = args.Contains("--sha1") || args.Contains("-S");
+            bool enableMD5 = args.Contains("--md5") || args.Contains("-M");
+
+            if (args.Contains("--all")) {
+                enableCRC = true;
+                enableSHA = true;
+                enableMD5 = true;
+            }
+
+            var file = args[args.Length - 1];
+
             var hasher = new Hasher();
 
-            hasher.Hash(args[0]);
+            hasher.Hash(file, enableCRC, enableSHA, enableMD5);
+            return 0;
         }
     }
 }
