9e6d87d9ca05b75aa9e45fa316caa017d8827968
SnowKit <funniray10@gmail.com>
2025-10-11 20:24:35 +0100

    http stream

--- Hash.cs
+++ Hash.cs
@@ -1,5 +1,11 @@
 using Force.Crc32;
+using System.Buffers.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace HashTest {
     class Hasher {
@@ -19,72 +25,93 @@ namespace HashTest {
             timers[timerIndex] += timer.ElapsedMilliseconds;
         }
 
-        public void Hash(string filename, bool crcEnabled, bool sha1Enabled, bool md5Enabled) {
+        public async Task Hash(string filename, bool crcEnabled, bool sha1Enabled, bool md5Enabled) {
             timers = new double[3]{0,0,0};
             var timer = System.Diagnostics.Stopwatch.StartNew();
-            using (var stream = File.Open(filename, FileMode.Open)) {
-                using (var reader = new BinaryReader(stream)) {
-                    var tasks = new List<Task>();
-                    var hashes = new System.Collections.Concurrent.ConcurrentDictionary<int, byte[]>();
-                    var i = 0;
+            var url = new Uri(filename);
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(url.UserInfo)));
+            var result = await client.GetStreamAsync(url);
+            // var stream = await result.Content.ReadAsStreamAsync();
+            long bytesRead = 0;
+            using (var reader = new BinaryReader(result)) {
+                var tasks = new List<Task>();
+                var hashes = new System.Collections.Concurrent.ConcurrentDictionary<int, byte[]>();
+                var i = 0;
 
-                    using var crc = new Crc32Algorithm();
-                    using var sha1 = SHA1.Create();
-                    using var md5 = MD5.Create();
+                using var crc = new Crc32Algorithm();
+                using var sha1 = SHA1.Create();
+                using var md5 = MD5.Create();
 
-                    Task crcTask = Task.Run(()=>{});
-                    Task sha1Task = Task.Run(()=>{});
-                    Task md5Task = Task.Run(()=>{});
-                    
-                    while (true) {
-                        byte[] read = reader.ReadBytes(ED2K_BLOCK_LENGTH);
-                        Task.WaitAll(new Task[3]{crcTask, sha1Task, md5Task});
+                Task crcTask = Task.Run(()=>{});
+                Task sha1Task = Task.Run(()=>{});
+                Task md5Task = Task.Run(()=>{});
 
-                        if (read.Length <= 0) {break;}
-                        var localIndex = i;
-                        var isLast = read.Length < ED2K_BLOCK_LENGTH;
-                    
-                        if (crcEnabled)
-                            crcTask = Task.Run(()=>{hashBlock(crc, read, isLast, 0);});
-                        if (sha1Enabled)
-                            sha1Task = Task.Run(()=>{hashBlock(sha1, read, isLast, 1);});
-                        if (md5Enabled)
-                            md5Task = Task.Run(()=>{hashBlock(md5, read, isLast, 2);});
+                while (true)
+                {
+                    byte[] read = reader.ReadBytes(ED2K_BLOCK_LENGTH);
+                    Task.WaitAll(new Task[3] { crcTask, sha1Task, md5Task });
 
-                        tasks.Add(Task.Run(()=>{
-                            MD4 md4 = new MD4();
-                            hashes.TryAdd(localIndex, md4.GetByteHashFromBytes(read));
-                        }));
-
-                        i++;
-                    }
-
-                    Console.WriteLine("Finished reading file in " + timer.ElapsedMilliseconds + "ms");
-                    Task.WaitAll(tasks.ToArray());
-                    var sortedHashes = new SortedList<int, byte[]>(hashes);
-                    var digests = new byte[0];
-
-                    foreach (byte[] subhash in sortedHashes.Values) {
-                        digests = digests.Concat(subhash).ToArray();
-                    }
-
-                    MD4 md4 = new MD4();
-                    string hash = md4.GetHexHashFromBytes(digests);
-
-                    long time = timer.ElapsedMilliseconds;
-
-                    var file = new FileInfo(filename);
+                    if (read.Length <= 0) { break; }
+                    var localIndex = i;
+                    var isLast = read.Length < ED2K_BLOCK_LENGTH;
+                    bytesRead += read.Length;
 
                     if (crcEnabled)
-                        Console.WriteLine("CRC32: {0} (took {1}ms)", MD4.BytesToHex(crc.Hash, null), timers[0]);
+                        crcTask = Task.Run(() => { hashBlock(crc, read, isLast, 0); });
                     if (sha1Enabled)
-                        Console.WriteLine("SHA1: {0} (took {1}ms)", MD4.BytesToHex(sha1.Hash, null), timers[1]);
+                        sha1Task = Task.Run(() => { hashBlock(sha1, read, isLast, 1); });
                     if (md5Enabled)
-                        Console.WriteLine("MD5: {0} (took {1}ms)", MD4.BytesToHex(md5.Hash, null), timers[2]);
-                    Console.WriteLine("ED2K: {0}", hash);
+                        md5Task = Task.Run(() => { hashBlock(md5, read, isLast, 2); });
 
-                    Console.WriteLine("Overall speed: {0}MB/s in {1}ms", Math.Round((file.Length/(1024*1024))/(time/1000.0), 2), time);
+                    tasks.Add(Task.Run(() =>
+                    {
+                        MD4 md4 = new MD4();
+                        hashes.TryAdd(localIndex, md4.GetByteHashFromBytes(read));
+                    }));
+
+                    i++;
                 }
+                
+                Task.WaitAll(tasks.ToArray());
+                var sortedHashes = new SortedList<int, byte[]>(hashes);
+                var digests = new byte[0];
+
+                foreach (byte[] subhash in sortedHashes.Values) {
+                    digests = digests.Concat(subhash).ToArray();
+                }
+
+                MD4 md4 = new MD4();
+                string hash = md4.GetHexHashFromBytes(digests);
+
+                long time = timer.ElapsedMilliseconds;
+
+                // var file = new FileInfo(filename);
+
+                HashOutput output = new();
+                output.timers = new();
+
+                if (crcEnabled) 
+                {
+                    output.crc32 = MD4.BytesToHex(crc.Hash, null);
+                    output.timers.crc32 = timers[0];
+                }
+                if (sha1Enabled)
+                {
+                    output.sha1 = MD4.BytesToHex(sha1.Hash, null);
+                    output.timers.sha1 = timers[1];
+                }
+                if (md5Enabled)
+                {
+                    output.md5 = MD4.BytesToHex(md5.Hash, null);
+                    output.timers.md5 = timers[2];
+                }
+
+                output.ed2k = hash;
+                output.speedMBps = Math.Round((bytesRead / (1024 * 1024)) / (time / 1000.0), 2);
+                output.timeMs = time;
+
+                Console.WriteLine(JsonSerializer.Serialize(output, HashOutputContex.Default.HashOutput));
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
-﻿namespace HashTest {
+﻿using System.Dynamic;
+using System.Runtime.Intrinsics.Arm;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HashTest {
+    public class HashOutput
+    {
+        public string? crc32 { get; set; }
+        public string? sha1 { get; set; }
+        public string? md5 { get; set; }
+        public string? ed2k { get; set; }
+
+        public HashTimers? timers { get; set; }
+        public double? timeMs { get; set; }
+        public double speedMBps { get; set; }
+        public string? error { get; set; }
+    }
+
+    public class HashTimers
+    {
+        public double? crc32 { get; set; }
+        public double? sha1 { get; set; }
+        public double? md5 { get; set; }
+    }
+
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    [JsonSerializable(typeof(HashOutput))]
+    public partial class HashOutputContex : JsonSerializerContext { }
+
     class Program {
-        static int Main(string[] args) {
+        static async Task<int> Main(string[] args) {
             if (args.Length == 0) {
                 Console.WriteLine("Usage: testFile [--crc | -C] [--sha1 | -S] [--md5 | -M] [--all] file");
                 return 1;
@@ -19,8 +49,16 @@
             var file = args[args.Length - 1];
 
             var hasher = new Hasher();
-
-            hasher.Hash(file, enableCRC, enableSHA, enableMD5);
+            try
+            {
+                await hasher.Hash(file, enableCRC, enableSHA, enableMD5);
+            } catch (Exception e)
+            {
+                HashOutput output = new();
+                output.error = e.ToString();
+                Console.Error.WriteLine(JsonSerializer.Serialize(output, HashOutputContex.Default.HashOutput));
+                return 1;
+            }
             return 0;
         }
     }
